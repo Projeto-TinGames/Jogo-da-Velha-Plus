3859d3916f20e9f9f7994b275172b701f4e990d7
RafaelPetr <petryrafael29@gmail.com>
2020-10-04 19:44:57 -0300

    Semana 1 - Adaptando o código parte 2
    -Adicionado suporte para tabuleiros maiores;
    -Finalizada a lógica para testes diagonais;

--- Jogo da Velha+ algumas melhorias.py
+++ Jogo da Velha+ algumas melhorias.py
@@ -6,14 +6,20 @@ import random
 
 pygame.init()
 
-#Pede a quantidade de colunas e linhas do tabuleiro e calcula o número de casas (apenas conceitual, então coloco automaticamente em 3x3)
+tamanho = [600,600]
+
 quantidadeColunas = int(input("Digite a quantidade de colunas: "))
 quantidadeLinhas = int(input("Digite a quantidade de linhas: "))
-quantidadeColunas = 3
-quantidadeLinhas = 3
+
 quantidadeCasas = quantidadeColunas*quantidadeLinhas
 
-tela = pygame.display.set_mode((600, 600), 0, 32)
+distanciaLinhasHorizontais = tamanho[0]//quantidadeColunas
+distanciaLinhasVerticais = tamanho[1]//quantidadeLinhas
+
+centroDaCasaX = distanciaLinhasHorizontais//2
+centroDaCasaY = distanciaLinhasVerticais//2
+
+tela = pygame.display.set_mode(tamanho, 0, 32)
 
 pygame.display.set_caption("Jogo da Velha")
 
@@ -30,20 +36,19 @@ class Casa:
 
 #Cria uma matriz vazia para preencher com casas
 casas = []
-
 #Preenche a matriz com casas
 for l in range(quantidadeLinhas):
     casas.append([None]*quantidadeColunas)
     for c in range(quantidadeColunas):
-        rect = Rect((200*c, 200*l), (200, 200))
+        rect = Rect((distanciaLinhasHorizontais*c, distanciaLinhasVerticais*l), (distanciaLinhasHorizontais, distanciaLinhasVerticais))
         casas[l][c] = Casa(rect)
 
-def desenhar_tabuleiro():
-    #Desenha o tabuleiro com base nas linhas e colunas do tabuleiro
+def desenhar_tabuleiro():#Desenha o tabuleiro com base nas linhas e colunas do tabuleiro
+                                                 
     for l in range(1,len(casas)):
-        pygame.draw.line(tela, (255, 255, 255), (0, l*200), (600, l*200), 10)
-    for c in range(1,len(casas[l])):
-        pygame.draw.line(tela, (255, 255, 255), (c*200, 0), (c*200, 600), 10)
+        pygame.draw.line(tela, (255, 255, 255), (0, l*distanciaLinhasVerticais), (tamanho[1], l*distanciaLinhasVerticais), 10)
+    for c in range(1,len(casas[0])):
+        pygame.draw.line(tela, (255, 255, 255), (c*distanciaLinhasHorizontais, 0), (c*distanciaLinhasHorizontais, tamanho[0]), 10)
 
 def testa_pos(mouse_pos,jogador):
     #Testa se a casa selecionada é válida
@@ -51,15 +56,15 @@ def testa_pos(mouse_pos,jogador):
     for l in range(len(casas)):
         for c in range(len(casas[l])):
             if (casas[l][c].rect.collidepoint(mouse_pos) and casas[l][c].valor == ''):
-                atualiza_tabuleiro(jogador,casas[l][c],[100+200*c,100+200*l])
+                atualiza_tabuleiro(jogador,casas[l][c],[centroDaCasaX+distanciaLinhasHorizontais*c,centroDaCasaY+distanciaLinhasVerticais*l])
 
 def atualiza_tabuleiro(jogador,casa,pos):
     global jogadores,turno,quantidadeCasas,casasPreenchidas
     
     #Pega a imagem do jogador que jogou e desenha
     imgJogador = pygame.image.load(jogadores[turno]+".png").convert_alpha()
-    imgJogador = pygame.transform.scale(imgJogador, (100, 100))
-    tela.blit(imgJogador, (pos[0] - 50, pos[1] - 50))
+    imgJogador = pygame.transform.scale(imgJogador, (centroDaCasaX, centroDaCasaY))
+    tela.blit(imgJogador, (pos[0] - centroDaCasaX//2, pos[1] - centroDaCasaY//2))
     
     casa.valor = jogador #Altera o valor da casa para a string do jogador (X ou O)
     
@@ -80,7 +85,7 @@ def atualiza_tabuleiro(jogador,casa,pos):
     if (turno == len(jogadores)):
         turno = 0
     
-def teste_vitoria(valor): #Testa as possibilidades de vitória (precisa de algumas mudanças, mas essa é a lógica)
+def teste_vitoria(valor): #Testa as possibilidades de vitória
     return teste_horizontal(valor) or teste_vertical(valor) or teste_diagonal(valor)
 
 def teste_horizontal(valor): #Pra cada linha ver o valor de cada coluna e comparar com o valor do jogador
@@ -105,24 +110,29 @@ def teste_vertical(valor): #Pra cada coluna ver o valor de cada linha e comparar
             if (contador == 3):
                 return True 
 
-def teste_diagonal(valor): #Precisa de melhorias, mas funciona em 3x3
+def teste_diagonal(valor):
     contador = 0
-    for l in range(len(casas)):
-        if (casas[l][l].valor == valor):
-            contador += 1
-        else:
-            contador = 0
-        if (contador == 3):
-            return True 
-
+    for c in range(len(casas[0])-2):
+        for l in range(len(casas)):
+            if (l+c < len(casas[0])):
+                if (casas[l][l+c].valor == valor):
+                    contador += 1
+                else:
+                    contador = 0
+                if (contador == 3):
+                    return True
+                
     contador = 0
-    for l in range(len(casas)):
-        if (casas[l][len(casas)-l-1].valor == valor):
-            contador += 1
-        else:
-            contador = 0
-        if (contador == 3):
-            return True
+    for c in range(len(casas[0])-2):
+        for l in range(len(casas)):
+            if (l+c < len(casas[0])):
+                if (casas[l][len(casas[0])-1-l-c].valor == valor):
+                    contador += 1
+                else:
+                    contador = 0
+                if (contador == 3):
+                    return True 
+        
 
 while True:
     for e in pygame.event.get():
