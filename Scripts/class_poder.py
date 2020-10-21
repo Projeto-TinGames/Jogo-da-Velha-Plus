7ae48de511b1d8ad27ad9303340a87bcbcfb0d96
RafaelPetr <petryrafael29@gmail.com>
2020-10-21 13:46:48 -0300

    Semana 3 - Adicionando poderes pt.2
    -Arrumado um erro no poder de troca em que os valores que mudavam eram duplicados;

--- Scripts/class_poder.py
+++ Scripts/class_poder.py
@@ -18,12 +18,18 @@ class Troca(Poder):
     def executa_poder(self,l,c):
         super().executa_poder(l,c)
 
+        jogadores = manager.jogadores.copy()
+
         jogadoresMudar = manager.jogadores.copy()
         jogadoresMudarPara = manager.jogadores.copy()
 
         for i in range(len(jogadoresMudarPara)):
             while jogadoresMudar[i] == jogadoresMudarPara[i]:
-                jogadoresMudarPara[i] = jogadoresMudar[random.randint(0,len(jogadoresMudar)-1)]
+                jogadoresMudarPara[i] = jogadores[random.randint(0,len(jogadores)-1)]
+                jogadores.remove(jogadoresMudarPara[i])
+
+        for i in range(len(jogadoresMudarPara)):
+            print (jogadoresMudar[i].valor + " = " + jogadoresMudarPara[i].valor)
 
         for l in range(len(manager.tabuleiro.casas)):
             for c in range(len(manager.tabuleiro.casas[l])):
